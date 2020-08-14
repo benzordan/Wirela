660e986844b0cbcd978be087c5c6871ade77c2af
coppar <pohsiyu@gmail.com>
2020-08-14 22:35:12 +0800

    Revert "Revert "Revert "Revert "Merge branch 'master' of https://github.com/benzordan/Wirela""""
    This reverts commit 69263e8d5523dcf4bdf79b4be34965c96d60818d.

---
+++ src/controller/productController.js
@@ -0,0 +1,38 @@
+import { ModelProduct } from '../models/models';
+import { downloadResource } from '../utils/util';
+
+const controller = {};
+
+controller.download = async (req, res) => {
+    const fields = [
+    {
+        label: 'Product UUID',
+        value: 'uuidProduct'
+    },
+    {
+        label: 'Product Name',
+        value: 'name'
+    },
+    {
+        label: 'Category',
+        value: 'category'
+    },
+    {
+        label: 'Description',
+        value: 'description'
+    },
+    {
+        label: 'Quantity',
+        value: 'quantity'
+    },
+    {
+        label: 'Price',
+        value: 'price'
+    }
+    ];
+    const data = await ModelProduct.findAll();
+
+    return downloadResource(res, 'products.csv', fields, data);
+}
+
+export default controller;
--- src/routes/admin/admin.js
+++ src/routes/admin/admin.js
@@ -1,11 +1,11 @@
 import { Router, Request, Response, NextFunction } from 'express'
-import { UserRole } from '../../../build/models/users';
-import { ModelOrder } from '../../models/orders';
+import { UserRole, ModelOrder, ModelProduct, ModelUser } from '../../models/models';
+import { flash_message, FlashType  } from '../../helpers/flash-messenger';
+import  productController from '../../controller/productController';
+import { getPagination, getPagingData } from '../../controller/paginationController';
+import { upload, remove_file, imageUrl} from '../../helpers/imageUpload';
+import { Op } from 'sequelize';
 
-/**
- * Configure router parameters
- * @see "http://expressjs.com/en/5x/api.html#express.router"
- */
 const router = Router({
 	caseSensitive: false,   //	Ensure that /home vs /HOME does exactly the same thing
 	mergeParams  : false,   //	Cascade all parameters down to children routes.
@@ -13,22 +13,14 @@ const router = Router({
 });
 
 /**
- * 
- * ==================
- * Admin Order Routes
- * ==================
- * /admin/orders
- * /admin/orders/:orderId
- * ===================
+ * Sub-routes
  */
-
-router.use('/services',          authorizer, require("./services/services"));
-router.get('/manage-products',   authorizer, page_manage_products);
-router.get("/manage-users",      authorizer, page_manage_users);
+router.use('/product',     require('./services/products'));
 
 // Admin Order Routes
 router.get('/orders', page_order_list)
 router.get('/orders/:orderId', page_order_item)
+router.get('/invoice', page_invoice)
 module.exports = router;
 
 /**
@@ -38,33 +30,13 @@ module.exports = router;
  * @param {NextFunction} next 
  */
 
+
 // Modify the authorizer so that it checks if the user role is a staff
 function authorizer(request, response, next) {
 	if (request.user.role === UserRole.Admin)
 		return next();
 	else
 		return response.status(403);
-}
-
-/**
- * 
- * @param {Request}  request  Incoming HTTP Request
- * @param {Response} response Outgoing HTTP Response
- */
-function page_manage_products(request, response) {
-	response.status(200).json({
-		"message": "okay"
-	});
-}
-/**
- * 
- * @param {Request}  request  Incoming HTTP Request
- * @param {Response} response Outgoing HTTP Response
- */
-function page_manage_users(request, response) {
-	response.status(200).json({
-		"message": "okay"
-	});
 }
 
 // /admin/orders
@@ -74,13 +46,22 @@ function page_manage_users(request, response) {
  * @param {Request} request Incoming HTTP Request
  * @param {Response} response Outgoing HTTP 
  */
+
+
+async function page_order_item(req, res)
+
+
 async function page_order_list(req, res) {
 	try {
-		const videos = await ModelOrder.findAll({
+		const orders = await ModelOrder.findAll({
+			order: [
+				["name", "ASC"]
+			],
 			raw: true
 		})
-		return res.render('staff/orderList', {
-			videos: videos
+		return res.render('staff/orders/orderList', {
+			layout: "staff",
+			pageCSS: "/css/staff/....",
 		})
 	}
 	catch (error) {
@@ -113,8 +94,32 @@ async function page_order_item(req, res) {
 		}
 	}
 	catch {
-		console.error(`Failed to retrieve video ${req.params["orderId"]}`);
+		console.error(`Failed to retrieve order ${req.params["orderId"]}`);
 		console.error(error);
 		return res.status(500).end();
 	}
 }
+async function page_invoice(req, res) {
+	try {
+		const content = await ModelUser.findOne({
+			// Find a product according to req.params["uuid"]
+	});
+		if (content) {
+			return res.render('staff/orders/invoice', {
+				title: "wirela staff: invoice",
+				layout: "staff",
+				"content": content,
+			});
+		}
+		else {
+			console.error(`Failed to retrieve product ${req.params["uuidProduct"]}`);
+			console.error("error");
+			return res.status(410).end();
+		}
+	} catch (error) {
+		console.log("Internal server error")
+		console.error(error)
+		return res.status(500).end()
+	}
+}
+
